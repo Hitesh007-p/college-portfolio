43c52f0ff8653fea45fdaac0481ece1387932528
Hitesh Patil <hiteshpatil4000@gmail.com>
2024-12-19 14:58:06 +0530

    add style

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,9 +13,9 @@ export default function Hero() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className="text-center text-black"
+        className="text-6xl font-bold mb-4 bg-gradient-to-r from-red-400 to-orange-800 text-transparent bg-clip-text text-center relative -top-20"
       >
-        <h1 className="text-6xl font-bold mb-4">Welcome to Nutan jr. College</h1>
+        <h1 className="text-6xl font-bold mb-4 ">Welcome to Nutan jr. College</h1>
         <p className="text-2xl">Shaping the future through education</p>
       </motion.div>
     </section>
